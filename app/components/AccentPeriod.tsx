f19e56cc43fa313ea8c27101c8fbc44752832bcb
pathlyaics-gif <pathly.ai.cs@gmail.com>
2026-06-15 20:49:48 +0800

    v

--- app/components/AccentPeriod.tsx
+++
@@ -1,6 +0,0 @@
-import { colors } from "../lib/tokens";
-
-// Renders the final period of a headline in terracotta.
-export default function AccentPeriod() {
-  return <span style={{ color: colors.accent }}>.</span>;
-}
